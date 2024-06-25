65bebd4b2a3dbf2ec61decfff6f40a51ce37f3ef
victudor09 <victudor09@gmail.com>
2024-06-25 22:06:01 +0200

    vacio

--- index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-const PORT = 3000;
-
-app.use(express.json());
-
-app.use("/producto", require("./routes/producto"));
-
-app.listen(PORT, () => console.log("Servidor levantado en el puerto " + PORT));
--- routes/producto.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const ProductoController = require("../controllers/ProductoController");
-
-router.post("/", ProductoController.create);
-
-module.exports = router;
