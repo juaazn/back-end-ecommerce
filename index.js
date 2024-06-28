150ea65ffb5db104fa264dd7aa0e8476914cdc7d
chuans <juaazn.05@gmail.com>
2024-06-28 13:44:55 +0200

    EndPoint categoria added

--- index.js
+++ index.js
@@ -5,5 +5,6 @@ const PORT = 3000;
 app.use(express.json());
 
 app.use("/producto", require("./routes/producto"));
+app.use("/categoria", require("./routes/categoria"));
 
 app.listen(PORT, () => console.log("Servidor levantado en el puerto " + PORT));
