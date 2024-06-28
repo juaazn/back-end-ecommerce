7c5ceb9c11da31815a86702bfa0c06bc9a764526
chuans <juaazn.05@gmail.com>
2024-06-28 13:12:34 +0200

    Change categorie name

--- controllers/CategoriaController.js
+++ controllers/CategoriaController.js
@@ -3,12 +3,14 @@ const { Categoria, Producto } = require("../models/index.js");
 const CategoriaController = {
   create(req, res) {
     req.body.role = "categoria";
-    Producto.create(req.body)
+    const { nombre_categoria } = req.body;
+
+    Producto.create(nombre_categoria)
       .then((categoria) => {
         res
           .status(201)
-          .send({ message: "Categoria creada con éxito", categoria })
-        })
+          .send({ message: "Categoria creada con éxito", categoria });
+      })
       .catch((err) => console.error(err));
   },
 
@@ -16,12 +18,12 @@ const CategoriaController = {
     Categoria.findAll({ include: [Producto] })
       .then((categorias) => res.send(categorias))
       .catch((err) => {
-        console.log(err)
+        console.log(err);
         res.status(500).send({
-            message: 'Ha habido un problema al cargar las categorias',
-          })
-      })
-  }
+          message: "Ha habido un problema al cargar las categorias",
+        });
+      });
+  },
 };
 
 module.exports = CategoriaController;
