25e76a3fa2574a1409fea2e6a6a2637b4946d481
chuans <juaazn.05@gmail.com>
2024-06-29 20:56:00 +0200

    Endpoint login added

--- routes/usuario.js
+++ routes/usuario.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const UsuarioController = require("../controllers/UsuarioController");
 
 router.post("/create", UsuarioController.create);
+router.post("/login", UsuarioController.login);
 
 module.exports = router;
