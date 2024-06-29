f4427383c0bbc2618736c80a9e85ac07000a0394
chuans <juaazn.05@gmail.com>
2024-06-29 17:46:58 +0200

    bcryptjs

--- controllers/UsuarioController.js
+++ controllers/UsuarioController.js
@@ -4,8 +4,9 @@ const bcrypt = require("bcryptjs");
 const UsuarioController = {
   create(req, res) {
     const { name, email, password, role } = req.body;
+    const passwordHash = bcrypt.hashSync(password, 10);
 
-    Usuario.create({ name, email, password, role })
+    Usuario.create({ name, email, password: passwordHash, role })
       .then((usuario) => {
         res.status(201).send({ message: "Usuario creada con éxito", usuario });
       })
