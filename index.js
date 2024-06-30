d0ff6a64e63b4175897591ed1cce3c905877fcfd
chuans <juaazn.05@gmail.com>
2024-06-30 15:11:36 +0200

    Middleware type error  added

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+const { typeError } = require("./middlewares/errors");
+
 app.use(express.json());
 
 app.use("/usuario", require("./routes/usuario"));
 app.use("/producto", require("./routes/producto"));
 app.use("/categoria", require("./routes/categoria"));
+
+app.use(typeError);
 
 app.listen(PORT, () => console.log("Servidor levantado en el puerto " + PORT));
