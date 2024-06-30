bb1e7772dddea849ab5bdbfd548ab6c5cac1b03d
chuans <juaazn.05@gmail.com>
2024-06-30 14:47:13 +0200

    Route logout added

--- routes/usuario.js
+++ routes/usuario.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const UsuarioController = require("../controllers/UsuarioController");
+const { authentication } = require("../middlewares/authentication");
 
 router.post("/create", UsuarioController.create);
 router.post("/login", UsuarioController.login);
+router.delete("/logout", authentication, UsuarioController.logout);
 
 module.exports = router;
