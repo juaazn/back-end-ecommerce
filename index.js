13659c340fc91dd02d41b6d17cda85c291c7299b
chuans <juaazn.05@gmail.com>
2024-06-30 16:11:48 +0200

    Commit merge develop

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
-const { typeError } = require('./middlewares/errors')
-
 const { typeError } = require("./middlewares/errors");
 
 app.use(express.json());
@@ -12,9 +10,6 @@ app.use("/producto", require("./routes/producto"));
 app.use("/categoria", require("./routes/categoria"));
 app.use("/pedido", require("./routes/pedido"));
 
-app.use(typeError) //EJECUTAR MIDDLEWARE GESTION ERRORES
-
-
-app.use(typeError);
+app.use(typeError); //EJECUTAR MIDDLEWARE GESTION ERRORES
 
 app.listen(PORT, () => console.log("Servidor levantado en el puerto " + PORT));
