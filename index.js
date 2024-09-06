4b16e600c81a83c1e421526550266d2fbfb1b345
victudor09 <victudor09@gmail.com>
2024-09-06 19:57:56 +0200

    cors victor

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require("express");
+const cors = require('cors')
 const app = express();
 const PORT = 3000;
 const { typeError } = require("./middlewares/errors");
 
 app.use(express.json());
+
+
+app.use(cors())
+
 
 app.use("/usuario", require("./routes/usuario"));
 app.use("/producto", require("./routes/producto"));
